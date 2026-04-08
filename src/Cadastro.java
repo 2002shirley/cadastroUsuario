56af62f913503dcbd932ddd7b8c16e75a68ef55a
Darce-Me <discarnivora@gmail.com>
2026-04-08 20:10:58 -0300

    Atributos e construtor

--- src/Cadastro.java
+++ src/Cadastro.java
@@ -1,5 +1,16 @@
-
-
 public class Cadastro {
+    
+    private String nome;
+    private int idade;
+    private String cpf;
 
+    public Cadastro(String nome, int idade, String cpf) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+    
+    
+    
+    
 }
